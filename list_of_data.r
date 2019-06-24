67b09d2fcc558543867633743cd376b7bf0d5727
dhan0779 <45704182+dhan0779@users.noreply.github.com>
2019-06-24 14:43:12 -0400

    Add files via upload

---
+++ list_of_data.r
@@ -0,0 +1,17 @@
+List of Data Sets
+
+Data Set 1
+R: data(state)
+
+Data Set 2
+R:
+library(dplyr)
+data(nasa)
+
+Data Set 3
+R:
+library(dplyr)
+data(storms)
+
+Data Set 4
+UCI DataBase: http://archive.ics.uci.edu/ml/datasets/Soybean+%28Large%29
